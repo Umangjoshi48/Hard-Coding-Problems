e4ea8b9127ee9d91891bb6af2ab9a92d67ffcbb6
Umang <47896558+Umangjoshi48@users.noreply.github.com>
2025-05-30 20:05:03 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -24,124 +24,96 @@ class Calculator {
             if (str[i] == " ") {
                 continue;
             }
-            else 
-            {
-                if (!isNaN(parseInt(str[i])))    
-                {
-                    let range=1;
+            else {
+                if (!isNaN(parseInt(str[i]))) {
+                    let range = 1;
                     let toPushString
-                    while(true)
-                    {
-                        if (!isNaN(parseInt(str[i + range])))
-                        {
+                    while (true) {
+                        if (!isNaN(parseInt(str[i + range]))) {
                             range++;
                         }
-                        else
-                        {
+                        else {
                             break;
                         }
                     }
-                    if (range==1)
-                    {
-                        toPushString=str[i];    
+                    if (range == 1) {
+                        toPushString = str[i];
                     }
-                    else
-                    {
-                        toPushString=str.slice(i,range);
-                        i=i+range;
+                    else {
+                        toPushString = str.slice(i, range);
+                        i = i + range;
                     }
                     strArray.push(toPushString);
                 }
-                else
-                {
+                else {
                     strArray.push(str[i]);
                 }
 
             }
 
         }
+        console.log(strArray);
         convertExpression(strArray);
     }
-} 
-function convertExpression(expression) 
-{
-    let postFix=[];
-    let stack=[];
-    associativityObject={
-        '+':1,
-        '-':1,
-        '*':2,
-        '/':2
+}
+function convertExpression(expression) {
+    let postFix = [];
+    let stack = [];
+    associativityObject = {
+        '+': 1,
+        '-': 1,
+        '*': 2,
+        '/': 2
     }
-    for (let i of expression) 
-        {
-        if (!isNaN(parseInt(i)))
-        { 
+    for (let i of expression) {
+        if (!isNaN(parseInt(i))) {
             postFix.push(i);
         }
-        else
-        {
-            if (stack.length==0)
-            {
+        else {
+            let lastElement = stack.at(-1);
+            if (stack.length == 0 || i == '(' || lastElement == '(') {
                 stack.push(i);
             }
-            else
-            {
-                let lastElement=stack.at(-1);
-                if (['(',')'].includes(lastElement))
-                {
-                    if (lastElement=='(')
-                    {
-                        stack.push(i);
-                    }
-                    else
-                    {
-                        while(true)
-                        {
-                            postFix.push(stack.pop());
-                            lastElement=stack.at(-1);
-                            if (lastElement==undefined || lastElement=='(')                                
-                                {
-                                    break;
-                                }
-                            else{
-                                continue;
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    while(true)
-                    {
-                        if (associativityObject[lastElement]>=associativityObject[i])
-                        {
-                            postFix.push(stack.pop());
-                            lastElement=stack.at(-1);
-                            if (lastElement==undefined || lastElement=='(')                                
-                                {
-                                    break;
-                                }
-                            else{
-                                continue;
-                            }              
-                        }
-                        else
-                        {
-                            stack.push(i)
+            else {
+                if (i == ')') {
+                    while (true) {
+                        postFix.push(stack.pop());
+                        lastElement = stack.at(-1);
+                        if (lastElement == undefined || lastElement == '(') {
                             break;
                         }
-                           
+                        else {
+                            continue;
+                        }
                     }
-                    
+                    stack.pop();
                 }
-
+                else {
+                    if (associativityObject[lastElement] > associativityObject[i]) {
+                        while (true) {
+                            postFix.push(stack.pop());
+                            lastElement = lastElement = stack.at(-1);
+                            if (associativityObject[lastElement] >= associativityObject[i]) {
+                                continue;
+                            }
+                            else {
+                                break;
+                            }
+                        }
+                        stack.push(i);
+                    }
+                    else {
+                        stack.push(i);
+                    }
+                }
             }
         }
-
     }
-    console.log(postFix);
+    postFix.push(stack.pop());
+    postFixString=postFix.join("");
+    console.log(postFixString);
 }
 const calc = new Calculator(0);
+console.log(`10 +   2 *    (   6 - (4 + 1) / 2) + 7`);
 Calculator.calculate(`10 +   2 *    (   6 - (4 + 1) / 2) + 7`);
 // console.log(calc.getResult()); 
