de3649992504d5ae5f6ffd81b2a49a993c4f74a6
Umang <47896558+Umangjoshi48@users.noreply.github.com>
2025-05-31 18:49:26 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -4,21 +4,32 @@ class Calculator {
     }
     add(num) {
         this.result = this.result + num;
+        return this.result;
     }
     subtract(num) {
         this.result = this.result - num;
+        return this.result;
     }
     multiply(num) {
         this.result = this.result * num;
+        return this.result;
     }
     divide(num) {
         this.result = this.result / num;
+        return this.result;
     }
     getResult() {
+        return this.result;
+    }
+    updateResult(num){
+        this.result = num;
+    }
+    clear()
+    {
+        this.result=0;
     }
     static calculate(str) {
         let strArray = [];
-
         for (let i = 0; i < str.length; i++) {
 
             if (str[i] == " ") {
@@ -52,7 +63,6 @@ class Calculator {
             }
 
         }
-        console.log(strArray);
         convertExpression(strArray);
     }
 }
@@ -110,10 +120,42 @@ function convertExpression(expression) {
         }
     }
     postFix.push(stack.pop());
-    postFixString=postFix.join("");
-    console.log(postFixString);
+    calculateExpression(postFix);
+}
+function calculateExpression(postFix) {
+    let stack = [];
+    for (let i = 0; i < postFix.length; i++) {
+        if (!isNaN(parseInt(postFix[i]))) {
+            stack.push(postFix[i]);
+        }
+        else {
+            let operand2 = parseFloat(stack.pop());
+            let operand1 = parseFloat(stack.pop());
+            stack.push(operation(postFix[i], operand1, operand2));
+        }
+    }
+}
+function operation(operator, operand1, operand2) {
+    let result;
+    calc.updateResult(operand1);
+    switch (operator) {
+        case '+':
+            result = calc.add(operand2);
+            break;
+        case '-':
+            result = calc.subtract(operand2);
+            break;
+        case '*':
+            result=calc.multiply(operand2);
+            
+            break;
+        case '/':
+            result = calc.divide(operand2);
+            break;
+        default:
+            console.log("not a valid operand")
+    }
+    return result;
 }
 const calc = new Calculator(0);
-console.log(`10 +   2 *    (   6 - (4 + 1) / 2) + 7`);
 Calculator.calculate(`10 +   2 *    (   6 - (4 + 1) / 2) + 7`);
-// console.log(calc.getResult()); 
